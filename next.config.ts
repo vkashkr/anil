0f737a22e567a4e53f2a8419d2ea332bb73e01c6
vkashkr <vk319301@gmail.com>
2026-01-17 03:57:37 +0530

    a

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // No output: 'export' for SSR
+  //output: 'export',
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
